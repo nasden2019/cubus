1011d69550f3fbef8ff48dd60becc72280897b72
Alex <alex@syloper.com>
2020-05-27 10:28:13 -0300

    cambio color navbar on scrolling

--- main.js
+++ main.js
@@ -6,22 +6,13 @@
 //     let fileName = $(this).val().split('\\').pop();
 //     $(this).next('.custom-file-label').addClass("selected").html(fileName);
 // });
-var i = 0;
-$(".navBarra")
-    .mouseover(function () {
-        i += 1;
-        $(this).find("span").text("mouse over x " + i);
-    })
-    .mouseout(function () {
-        $(this).find("span").text("mouse out ");
-    });
 
-var n = 0;
-$("div.enterleave")
-    .mouseenter(function () {
-        n += 1;
-        $(this).find("span").text("mouse enter x " + n);
-    })
-    .mouseleave(function () {
-        $(this).find("span").text("mouse leave");
-    });
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 80) {
+        $('.fixed-top').css('background', 'transparent');
+    } else {
+        $('.fixed-top').css('background', '#38033E');
+    }
+});
