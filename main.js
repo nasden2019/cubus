0f018f8a457ea02c9f02272c9639f4192d88dde3
Alex <alex@syloper.com>
2020-05-27 09:33:07 -0300

    main js

--- main.js
+++ main.js
@@ -6,3 +6,22 @@
 //     let fileName = $(this).val().split('\\').pop();
 //     $(this).next('.custom-file-label').addClass("selected").html(fileName);
 // });
+var i = 0;
+$(".navBarra")
+    .mouseover(function () {
+        i += 1;
+        $(this).find("span").text("mouse over x " + i);
+    })
+    .mouseout(function () {
+        $(this).find("span").text("mouse out ");
+    });
+
+var n = 0;
+$("div.enterleave")
+    .mouseenter(function () {
+        n += 1;
+        $(this).find("span").text("mouse enter x " + n);
+    })
+    .mouseleave(function () {
+        $(this).find("span").text("mouse leave");
+    });
