42bbb5d3a8e7af534cee2fe2d7c5a2af8d277360
Alex <alex@syloper.com>
2020-05-28 09:46:38 -0300

    scrolling nav

--- main.js
+++ main.js
@@ -7,21 +7,24 @@
 //     $(this).next('.custom-file-label').addClass("selected").html(fileName);
 // });
 
-
+// SCROLL NAVBAR DESKTOP: cambio de color, size and margin
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var logo = $('.logoNav');
+    var navLinks = $('.navbar-collapse');
     var link = $('.nav-link');
     if (scroll < 80) {
         $('.fixed-top').css('background', 'transparent');
         logo.css('margin', '10px');
-        link.css('font-size', '22px', 'padding-bottom', '5px');
-        $('.navBarra').css("height", "60px");
+        navLinks.css('margin-top', '15px');
+        link.css('font-size', '22px');
+        $('.navBarra').css("height", "90px");
     } else {
         $('.fixed-top').css('background', '#38033E');
-        logo.css('margin', '-100px');
-        link.css('font-size', '30px', 'padding-bottom', '60px');
-        $('.navBarra').css("height", "180px");
+        logo.css('margin', '-95px');
+        navLinks.css('margin-top', '-85px');
+        link.css('font-size', '30px');
+        $('.navBarra').css("height", "150px");
     }
 });
 
