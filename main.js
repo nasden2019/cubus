dc0d34c43cdd57fb88f45dfc83a6ae343fd26f9e
Alex <alex@syloper.com>
2020-05-28 14:04:11 -0300

    hover nav desktop

--- main.js
+++ main.js
@@ -28,14 +28,9 @@ $(window).scroll(function () {
     }
 });
 
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
-// } 
+// hamburger menu 
+$(document).ready(function () {
+    $('#nav-icon1').click(function () {
+        $(this).toggleClass('open');
+    });
+});
