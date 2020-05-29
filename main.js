26f2b2eb903dccd52d0e42b5eeeb1dfa3500c428
Alex <alex@syloper.com>
2020-05-29 13:41:00 -0300

    ultimas correcciones

--- main.js
+++ main.js
@@ -8,29 +8,29 @@
 // });
 
 // SCROLL NAVBAR DESKTOP: cambio de color, size and margin
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    var logo = $('.logoNav');
-    var navLinks = $('.navbar-collapse');
-    var link = $('.nav-link');
-    if (scroll < 80) {
-        $('.fixed-top').css('background', 'transparent');
-        logo.css('margin', '10px');
-        navLinks.css('margin-top', '15px');
-        link.css('font-size', '22px');
-        $('.navBarra').css("height", "90px");
-    } else {
-        $('.fixed-top').css('background', '#38033E');
-        logo.css('margin', '-95px');
-        navLinks.css('margin-top', '-85px');
-        link.css('font-size', '30px');
-        $('.navBarra').css("height", "150px");
-    }
-});
+// $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
+//     var logo = $('.logoNav');
+//     var navLinks = $('.navbar-collapse');
+//     var link = $('.nav-link');
+//     if (scroll < 80) {
+//         $('.fixed-top').css('background', 'transparent');
+//         logo.css('margin', '10px');
+//         navLinks.css('margin-top', '15px');
+//         link.css('font-size', '22px');
+//         $('.navBarra').css("height", "90px");
+//     } else {
+//         $('.fixed-top').css('background', '#38033E');
+//         logo.css('margin', '-95px');
+//         navLinks.css('margin-top', '-85px');
+//         link.css('font-size', '30px');
+//         $('.navBarra').css("height", "150px");
+//     }
+// });
 
 // hamburger menu 
-$(document).ready(function () {
-    $('#nav-icon1').click(function () {
-        $(this).toggleClass('open');
-    });
-});
+// $(document).ready(function () {
+//     $('#nav-icon1').click(function () {
+//         $(this).toggleClass('open');
+//     });
+// });
