a92fd3bd03b07a4a785e54371bf566a049f5fae6
Alex <alex@syloper.com>
2020-06-03 11:13:28 -0300

    seguros.php

---
+++ seguros.php
@@ -0,0 +1,175 @@
+<?php
+/*
+Template Name: seguros
+*/
+?>
+
+<?php get_header('2'); ?>
+
+<section
+            class="seccionVioleta d-none d-md-block mx-0 px-md-0 pt-md-0 pt-0 pb-0 accordion md-accordion accordion-1"
+            style="margin-top: 128px;" id="accordionEx123" role="tablist">
+            <div class="container-fluid">
+                <div class="row mx-0 px-0">
+                    <div class="col-12 col-md-12 px-0 text-center">
+                        <div class="row">
+                            <div class="col-md-4 px-0 d-flex align-items-center justify-content-center">
+                                <a class="btn violetBtn position-absolute my-auto boton" id="automotor"
+                                    data-toggle="collapse" href="#collapse19" aria-expanded="true"
+                                    aria-controls="collapse196">
+                                    Automotor
+                                </a>
+                                <div class="d-flex justify-content-center align-items-center position-absolute">
+                                    <div class="text-white position-absolute" style="display: none;"
+                                        id="automotorTexto">
+                                        <h2>Automotor</h2>
+                                        <p>RESP CIVIL</p>
+                                        <p>INCENDIO TOTAL Y/O PARCIAL</p>
+                                        <p>ROBO</p>
+                                        <p>DAÑOS TOTALES</p>
+                                        <p>CONSEJOS</p>
+                                        <p>ROBO</p>
+                                        <p>DAÑOS TOTALES</p>
+                                        <p>CONSEJOS</p>
+                                        <button class="botonSeguros">Contactar Asesor</button>
+                                    </div>
+                                </div>
+                                <img src="img/familiaDeskt.jpg" class="img-fluid imgPurple" alt="" id="automotorImg" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading196"
+                                    rl="tab"></div>
+                            </div>
+
+
+
+                            <!-- fin  primer div -->
+
+                            <div class="col-md-4 px-0 d-flex btn-efecto align-items-center justify-content-center">
+                                <a class="btn violetBtn position-absolute my-auto" id="Ética" data-toggle="collapse"
+                                    href="#collapse197" aria-expanded="true" aria-controls="collapse197">
+                                    Familiar
+                                </a>
+                                <img src="img/eticaDeskt.jpg" class="img-fluid imgPurple" alt="" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading197"
+                                    rl="tab"></div>
+                            </div>
+
+                            <div class="col-md-4 px-0 d-flex btn-efecto justify-content-center align-items-center">
+                                <a class="btn violetBtn position-absolute my-auto" id="Profesionalismo"
+                                    data-toggle="collapse" href="#collapse198" aria-expanded="true"
+                                    aria-controls="collapse198">
+                                    Accidentes Personales
+                                </a>
+                                <img src="img/pcDeskt.jpg" class="img-fluid imgPurple" alt="" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading198"
+                                    rl="tab"></div>
+                            </div>
+                            <!-- <div class="col-md-4 px-0 d-flex btn-efecto justify-content-center align-items-center">
+                                <a class="btn violetBtn position-absolute my-auto" id="Profesionalismo"
+                                    data-toggle="collapse" href="#collapse198" aria-expanded="true"
+                                    aria-controls="collapse198">
+                                    Industria y comercio
+                                </a>
+                                <img src="img/pcDeskt.jpg" class="img-fluid imgPurple" alt="" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading198"
+                                    rl="tab"></div>
+                            </div>
+                            <div class="col-md-4 px-0 d-flex btn-efecto justify-content-center align-items-center">
+                                <a class="btn violetBtn position-absolute my-auto" id="Profesionalismo"
+                                    data-toggle="collapse" href="#collapse198" aria-expanded="true"
+                                    aria-controls="collapse198">
+                                    Agro
+                                </a>
+                                <img src="img/pcDeskt.jpg" class="img-fluid imgPurple" alt="" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading198"
+                                    rl="tab"></div>
+                            </div>
+                            <div class="col-md-4 px-0 d-flex btn-efecto justify-content-center align-items-center">
+                                <a class="btn violetBtn position-absolute my-auto" id="Profesionalismo"
+                                    data-toggle="collapse" href="#collapse198" aria-expanded="true"
+                                    aria-controls="collapse198">
+                                    Empleadores
+                                </a>
+                                <img src="img/pcDeskt.jpg" class="img-fluid imgPurple" alt="" />
+                                <div class="px-0 d-flex justify-content-center align-items-center" id="heading198"
+                                    rl="tab"></div>
+                            </div> -->
+                        </div>
+
+
+
+                        <!-- accordion -->
+
+                        <div class="row px-0">
+                            <div class="col-md-4 px-0 align-items-center justify-content-center holis">
+                                <div id="collapse196" class="collapse" role="tabpanel" aria-labelledby="heading196"
+                                    data-parent="#accordionEx123">
+                                    <div class="d-flex justify-content-center align-items-center my-auto">
+                                        <p class="text-white position-absolute my-auto">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                            sed do eiusmod tempor incididunt ut labore et dolore magna
+                                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                            ullamco laboris
+                                        </p>
+
+                                        <img src="img/familiaDeskt.jpg" alt="" style="width: 100%; height: 40vh;"
+                                            class="overlay" />
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+                            <div class="col-md-4 px-0">
+                                <div id="collapse197" class="collapse" role="tabpanel" aria-labelledby="heading197"
+                                    data-parent="#accordionEx123">
+                                    <div class="d-flex justify-content-center align-items-center my-auto">
+                                        <p class="text-white w-100 position-absolute">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                            sed do eiusmod tempor incididunt ut labore et dolore magna
+                                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                            ullamco laboris
+                                        </p>
+
+                                        <img src="img/eticaDeskt.jpg" alt="" style="width: 100%; height: 40vh;"
+                                            class="overlay" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 px-0">
+                                <div id="collapse198" class="collapse" role="tabpanel" aria-labelledby="heading198"
+                                    data-parent="#accordionEx123">
+                                    <div class="d-flex justify-content-center align-items-center my-auto">
+                                        <p class="w-100 position-absolute">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                            sed do eiusmod tempor incididunt ut labore et dolore magna
+                                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                            ullamco laboris
+                                        </p>
+
+                                        <img src="img/chicaDeskIndex.jpg" alt="" style="width: 100%; height: 40vh;"
+                                            class="overlay" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- imagen azul -->
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- <div class="col-md-12 col-12"> -->
+                    <div class="" style="background-image: url('img/cinturon.jpg');"></div>
+                    <!-- <img src="img/familiaDeskt.jpg" alt=""> -->
+                    <!-- <div class="" style="background-image: url('img/pcDeskt.jpg');"> -->
+                    <!-- </div> -->
+                </div>
+            </div>
+        </section>
+
+        <?php get_footer(); ?>
+        <?php get_footer('2'); ?>
