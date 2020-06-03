e26d541f2f3cf0e8140302f181450a5e626bc872
Alex <alex@syloper.com>
2020-06-03 11:53:45 -0300

    jquery

--- seguros.php
+++ seguros.php
@@ -193,5 +193,56 @@ Template Name: segurosCubus
     </div>
 </section>
 
+
+<!-- JS SCRIPTS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+        </script>
+
+
+        <script>
+            $(document).ready(function () {
+                $('.boton#automotor').click(function () {
+                    $('#automotorImg').addClass('overlay');
+                    $(this).hide();
+                    $('#automotorTexto').show();
+                })
+                // $('.boton#familia').click(function () {
+                //     $('#familiaImg').addClass('overlay');
+                //     $(this).hide();
+                //     $('#familiaTexto').show();
+                // })
+                // $('.boton#automotor').click(function () {
+                //     $('#automotorImg').addClass('overlay');
+                //     $(this).hide();
+                //     $('#automotorTexto').show();
+                // })
+                // $('.boton#automotor').click(function () {
+                //     $('#automotorImg').addClass('overlay');
+                //     $(this).hide();
+                //     $('#automotorTexto').show();
+                // })
+                // $('.boton#automotor').click(function () {
+                //     $('#automotorImg').addClass('overlay');
+                //     $(this).hide();
+                //     $('#automotorTexto').show();
+                // })
+                // $('.boton#automotor').click(function () {
+                //     $('#automotorImg').addClass('overlay');
+                //     $(this).hide();
+                //     $('#automotorTexto').show();
+                // })
+            })
+            // function ok() {
+            //     $(".btn-efecto").fadeToggle(1000, function () {});
+            // }
+        </script>
+
 <?php get_footer(); ?>
 <?php get_footer('2'); ?>
