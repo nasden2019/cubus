a9120f8525bf577c1b4b2978c508c4dca34482da
Alex <alex@syloper.com>
2020-06-04 14:54:22 -0300

    faq

--- pFrecuentes.php
+++ pFrecuentes.php
@@ -16,6 +16,184 @@ Template Name: pFrecuentes
                 </div>
 
                 <div id="accordion">
+                            <div class="card mb-3">
+                                <div class="" id="headingOne">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseOne"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card mb-3">
+                                <div class="" id="headingTwo">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseTwo"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card mb-3">
+                                <div class="" id="headingThree">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseThree"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card mb-3">
+                                <div class="" id="headingFour">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseFour"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+                                <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card mb-3">
+                                <div class="" id="headingfive">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseFive"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+                                <div id="collapseFive" class="collapse" aria-labelledby="headingFive"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card mb-3">
+                                <div class="" id="headingSix">
+                                    <div class="mb-0">
+                                        <button class="btn icono" data-toggle="collapse" data-target="#collapseSix"
+                                            aria-expanded="true" aria-controls="collapseSix">
+                                            <i class="fas fa-plus fa-1x icono"></i>
+                                        </button>
+                                        <span class="ml-4">Qué es la ART?</span>
+                                    </div>
+                                </div>
+                                <div id="collapseSix" class="collapse" aria-labelledby="headingSix"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid.
+                                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                        quinoa nesciunt
+                                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                                        single-origin
+                                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes
+                                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                                        lomo. Leggings
+                                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                                        probably haven't heard
+                                        of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                <!-- <div id="accordion">
                     <div class="card mb-3">
                         <div class="" id="headingOne">
                             <div class="mb-0">
@@ -186,7 +364,7 @@ Template Name: pFrecuentes
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
