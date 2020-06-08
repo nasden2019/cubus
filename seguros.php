bc90263c1d1519513a3fff6149e802679b3466d6
Alex <alex@syloper.com>
2020-06-08 13:59:22 -0300

    ch

--- seguros.php
+++ seguros.php
@@ -479,6 +479,7 @@ Template Name: segurosCubus
 
 
 <!-- JS SCRIPTS -->
+
 <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
 </script>
@@ -488,6 +489,7 @@ Template Name: segurosCubus
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
     integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script> -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js">
 
 
 <script>
