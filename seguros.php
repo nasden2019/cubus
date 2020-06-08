b483be97607ebbf3bcce15954e762bd0811753a9
Alex <alex@syloper.com>
2020-06-08 15:27:58 -0300

    d

--- seguros.php
+++ seguros.php
@@ -104,7 +104,7 @@ Template Name: segurosCubus
 
                     <!-- segundo-->
                     <div class="col-md-4 px-0 d-flex align-items-center justify-content-center">
-                        <a class="btn violetBtn position-absolute my-auto boton" id="familia" data-toggle="collapse"
+                        <a class="btn violetBtn position-absolute my-auto boton familia" id="familia" data-toggle="collapse"
                             href="#collapse19" aria-expanded="true" aria-controls="collapse1">
                             Familia
                         </a>
@@ -113,7 +113,7 @@ Template Name: segurosCubus
                             <div class="text-white position-absolute" style="display: none; z-index: 1;"
                                 id="familiaTexto">
                                 <img src="<?php echo bloginfo('template_url' ); ?>/img/Boton.png"
-                                    class="position-absolute flechaSeg">
+                                    class="position-absolute flechaSeg familia">
                                 <h4 class="mb-md-4 mb-3 mt-md-5">Protegé tu hogar de manera integral</h4>
                                 <div class="mb-md-2 mb-1">Incendio edificio y mobiliario</div>
                                 <div class="mb-md-2 mb-1">Robo contenido</div>
@@ -546,11 +546,16 @@ Template Name: segurosCubus
             $('.automotor').toggle();
             $('#automotorTexto').toggle();
         })
-        $('.boton#familia').click(function () {
-            $('#familiaImg').addClass('overlay');
-            $(this).hide();
-            $('#familiaTexto').show();
+        $('.familia').click(function () {
+            $('#familiaImg').toggleClass('overlay');
+            $('.familia').toggle();
+            $('#familiaTexto').toggle();
         })
+        // $('.boton#familia').click(function () {
+        //     $('#familiaImg').addClass('overlay');
+        //     $(this).hide();
+        //     $('#familiaTexto').show();
+        // })
         $('.boton#accidentes').click(function () {
             $('#accidentesImg').addClass('overlay');
             $(this).hide();
