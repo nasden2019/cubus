1d7326b59d2cd365a51db40e2cb6e64191e59d61
Alex <alex@syloper.com>
2020-06-08 19:33:48 -0300

    autom

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     //     $('#automotorTexto').show();
     $('.automotor').click(function () {
         $('#automotorImg').toggleClass('overlay');
-        $('.automotor2').toggle();
+        $('.automotor').toggle();
         $('#automotorTexto').toggle();
     });
     $('.familia').click(function () {
