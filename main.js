29dd47ebfb8ee18a02426273575c9f9ae82620b6
Alex <alex@syloper.com>
2020-06-08 14:05:32 -0300

    data-mask

--- main.js
+++ main.js
@@ -55,10 +55,50 @@ $(window).scroll(function () {
 //     $("#menu-blanco").addClass("shadow-sm");
 // }
 
-$(document).ready(function () {
-    $('#birth-date').mask('00/00/0000');
-    $('#phone-number').mask('0000-0000');
-});
+Array.prototype.forEach.call(document.body.querySelectorAll("*[data-mask]"), applyDataMask);
+
+function applyDataMask(field) {
+    var mask = field.dataset.mask.split('');
+
+    // For now, this just strips everything that's not a number
+    function stripMask(maskedData) {
+        function isDigit(char) {
+            return /\d/.test(char);
+        }
+        return maskedData.split('').filter(isDigit);
+    }
+
+    // Replace `_` characters with characters from `data`
+    function applyMask(data) {
+        return mask.map(function (char) {
+            if (char != '_') return char;
+            if (data.length == 0) return char;
+            return data.shift();
+        }).join('')
+    }
+
+    function reapplyMask(data) {
+        return applyMask(stripMask(data));
+    }
+
+    function changed() {
+        var oldStart = field.selectionStart;
+        var oldEnd = field.selectionEnd;
+
+        field.value = reapplyMask(field.value);
+
+        field.selectionStart = oldStart;
+        field.selectionEnd = oldEnd;
+    }
+
+    field.addEventListener('click', changed)
+    field.addEventListener('keyup', changed)
+}
+
+// $(document).ready(function () {
+//     $('#birth-date').mask('00/00/0000');
+//     $('#phone-number').mask('0000-0000');
+// });
 
 // $("#date").inputmask({
 //     "mask": "(999) 999-9999"
