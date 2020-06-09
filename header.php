629319a42cb4cda9913a5111ff4b52e0bfe39c2f
Alex <alex@syloper.com>
2020-06-09 12:34:31 -0300

    botones seg

--- header.php
+++ header.php
@@ -52,18 +52,12 @@
                     <span></span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link text-white ml-3" href="#">EMPRESA</a>
-                </li>
-                <li class="nav-item">
                   <a class="nav-link text-white ml-3" href="<?php echo esc_url( home_url('/') ); ?>seguros">SEGUROS</a>
                 </li>
                 <li class="nav-item">
                   <!-- <a class="nav-link text-white ml-3" href="pFrecuentes.html">PREGUNTAS FRECUENTES</a> -->
                   <a class="nav-link text-white ml-3"
                     href="<?php echo esc_url( home_url('/') ); ?>pFrecuentes">PREGUNTAS FRECUENTES</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link text-white ml-3">R.R.H.H</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link text-white ml-3"
