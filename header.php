9d79eee95802f7644797f9dff599b601c441bf3d
Alex <alex@syloper.com>
2020-06-10 19:05:40 -0300

    sinks

--- header.php
+++ header.php
@@ -9,16 +9,15 @@
       integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?php echo bloginfo('template_url' ); ?>/webroot/css/style.min.css" />
     <link rel="stylesheet" href="<?php echo bloginfo('template_url' ); ?>/webroot/css/mixin.min.css" />
-    <link rel="stylesheet"
-      href="<?php echo bloginfo('template_url' ); ?>//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="<?php echo bloginfo('template_url' ); ?>/resources/demos/style.css">
+    <link rel="stylesheet" href="<?php echo bloginfo('template_url' ); ?>//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="<?php echo bloginfo('template_url' ); ?>/resources/demos/style.css">
 
 
     <link
       href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;0,900;1,400;1,600;1,800&display=swap"
       rel="stylesheet" />
 
-
+    
 
   </head>
 
@@ -77,6 +76,15 @@
         <ul class="nav navbar-nav mx-auto mt-5 item1">
           <!-- <ul class="nav navbar-nav mx-auto mt-5 item1"> -->
 
+          
+          <a class="navbar-brand" href="#">
+            <img src="<?php echo bloginfo('template_url' ); ?>/img/logo-mobile.png" class="img-fluid" alt=""
+              style="width: 215px;" />
+            <!-- ?= $this->Html->image('logo-mobile.png', ['class' => "img-fluid d-md-block", 'style' => 'width: 250px;']) ?> -->
+          </a>
+        </ul>
+
+        <div class="navbar navbar-expand-sm fixed-top test navBarra test navItem2">
           <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse"
             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
@@ -97,22 +105,13 @@
                 <a class="nav-link text-white ml-3" href="<?php echo esc_url( home_url('/') ); ?>seguros">SEGUROS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white ml-3" href="<?php echo esc_url( home_url('/') ); ?>pFrecuentes">PREGUNTAS
-                  FRECUENTES</a>
+                <a class="nav-link text-white ml-3" href="<?php echo esc_url( home_url('/') ); ?>pFrecuentes">PREGUNTAS FRECUENTES</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white ml-3" href="<?php echo esc_url( home_url('/') ); ?>contacto">CONTACTO</a>
               </li>
             </ul>
           </div>
-
-          <a class="navbar-brand" href="#">
-            <img src="<?php echo bloginfo('template_url' ); ?>/img/logo-mobile.png" class="img-fluid" alt=""
-              style="width: 215px;" />
-            <!-- ?= $this->Html->image('logo-mobile.png', ['class' => "img-fluid d-md-block", 'style' => 'width: 250px;']) ?> -->
-          </a>
-        </ul>
-
+        </div>
       </div>
-     
     </nav>
