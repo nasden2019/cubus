4b7663c2a43b18fdd0ca5c14e84a4eae01ebe021
Alex <alex@syloper.com>
2020-06-10 14:45:09 -0300

    mobile aut

--- seguros.php
+++ seguros.php
@@ -32,14 +32,13 @@ Template Name: segurosCubus
                                     <div class="">
                                         <h4 class="mb-md-4 mt-md-5 mb-3 d-none d-md-block">Cuidá tu vehículo de la forma
                                             mas conveniente</h4>
-                                        <h4 class="mb-md-4 mt-md-4 mb-3 d-block d-md-none">Cuidá tu vehículo</h4>
+                                        <h4 class="mb-md-4 mt-md-5 mb-2 d-block d-md-none">Cuidá tu vehículo</h4>
                                         <div class="mb-md-2 mb-1">Responsabilidad civil</div>
                                         <div class="mb-md-2 mb-1">Incendio total y/o parcial</div>
                                         <div class="mb-md-2 mb-1">Robo</div>
                                         <div class="mb-md-2 mb-1">Daños totales y/o parciales por accidente</div>
                                         <div class="mb-md-2 mb-1">Daños por granizo</div>
-                                        <div class="mb-md-4 mb-2">Beneficios extras y costos exclusivos para los
-                                            vehículos</div>
+                                        <div class="mb-md-4 mb-2">Beneficios extras y costos exclusivos</div>
                                         <!-- BOTON MODAL -->
                                         <button type="button" class="botonSeguros" data-toggle="modal"
                                             data-target="#exampleModal">Contactar asesor
