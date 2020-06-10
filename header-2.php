60598100c3f5320ac1a54c4a958efe87ab9ab023
Alex <alex@syloper.com>
2020-06-10 19:17:40 -0300

    navs

--- header-2.php
+++ header-2.php
@@ -75,7 +75,7 @@
 
         <!-- nav mobile -->
         <nav class="navbar navbar-expand-md fixed-top navBarra navItem1 footerVioleta px-md-5 pt-0 d-md-none d-block">
-            <div class="container-fluid cianColor pt-5">
+            <div class="container-fluid cianColor py-3">
                 <ul class="nav navbar-nav mx-auto mt-1 item1">
                     <!-- <ul class="nav navbar-nav mx-auto mt-5 item1"> -->
                     <a class="navbar-brand" href="#">
