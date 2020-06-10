28aa15f280c9d3de9917e40a0c1b5c8bcf9cb3a1
Alex <alex@syloper.com>
2020-06-10 11:54:30 -0300

    cont

--- contacto.php
+++ contacto.php
@@ -15,7 +15,9 @@ Template Name: contacto
                                     <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13393.206063450687!2d-60.65802!3d-32.943048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x91a29ca796372019!2sCubus%20Broker%20de%20Seguros!5e0!3m2!1sen!2sar!4v1591112958654!5m2!1sen!2sar"
                                         width="90%" height="500" frameborder="0" style="border:0;" allowfullscreen=""
-                                        aria-hidden="false" tabindex="0"></iframe>
+                                        aria-hidden="false" tabindex="0">
+                                    <img src="img/logo.jpg" alt="">
+                                </iframe>
                                 </div>
                             </div>
                             <!-- formulario-->
