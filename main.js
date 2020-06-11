e3a19dcd7017359e105c8a89379b4b4cdd1f529a
Alex <alex@syloper.com>
2020-06-11 07:29:37 -0300

    map

--- main.js
+++ main.js
@@ -166,7 +166,29 @@ $(document).ready(function () {
 })
 
 // disabled scroll mouse / map zoom in/out
-onclick = "document.getElementById('iframe').style.pointerEvents= 'auto'"
+var onMapMouseleaveHandler = function (event) {
+    var that = $(this);
+
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+}
+
+var onMapClickHandler = function (event) {
+    var that = $(this);
+
+    // Disable the click handler until the user leaves the map area
+    that.off('click', onMapClickHandler);
+
+    // Enable scrolling zoom
+    that.find('iframe').css("pointer-events", "auto");
+
+    // Handle the mouse leave event
+    that.on('mouseleave', onMapMouseleaveHandler);
+}
+
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.maps.embed-container').on('click', onMapClickHandler);
 
 
 // ICON MAP
