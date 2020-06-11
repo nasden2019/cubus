7a984b4646c7229c9ff4a76012adeb691e4347e8
Alex <alex@syloper.com>
2020-06-11 07:32:50 -0300

    contacto

--- contacto.php
+++ contacto.php
@@ -12,14 +12,9 @@ Template Name: contacto
                 <div class="row">
                     <div class="col-md-7 col-12 px-0 mb-4">
                         <div class="content-container  embed-container  maps">
-                            <iframe style="pointer-events: none;" src="https://www.google.com/maps/d/u/0/embed?mid=1whpmshyYMjxCRplNEZ4rmNsYFgTYHO7t"
-                                class="barraGris" width="90%" height="763" frameborder="0" style="border: 0";></iframe>
-                            <!-- <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13393.206063450687!2d-60.65802!3d-32.943048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x91a29ca796372019!2sCubus%20Broker%20de%20Seguros!5e0!3m2!1sen!2sar!4v1591112958654!5m2!1sen!2sar"
-                                        width="90%" height="500" frameborder="0" style="border:0;" allowfullscreen=""
-                                        aria-hidden="false" tabindex="0"> -->
-                            <!-- <img src="img/logo.jpg" alt=""> -->
-                            <!-- </iframe> -->
+                            <iframe style="pointer-events: none;"
+                                src="https://www.google.com/maps/d/u/0/embed?mid=1whpmshyYMjxCRplNEZ4rmNsYFgTYHO7t"
+                                class="barraGris" width="90%" height="763" frameborder="0" style="border: 0" ;></iframe>
                         </div>
                     </div>
                     <!-- formulario-->
