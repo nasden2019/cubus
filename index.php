e86c1d6cdae9a3b99f0a4b6d5588657a992f7a31
Alex <alex@syloper.com>
2020-06-12 15:53:47 -0300

    img

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                         </div> -->
       </div>
 
-      <div class="carousel-item desgraciaImg"
+      <div class="carousel-item desgraciaImg" role="listbox"
         style="background-image: url('<?php echo bloginfo('template_url' ); ?>/img/desgraciaHome.jpg');">
         <!-- style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')"> -->
       </div>
