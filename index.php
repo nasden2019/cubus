80158496f2135767d83ec96ad944e9a1dc2b7cad
Alex <alex@syloper.com>
2020-06-12 13:40:06 -0300

    index

--- index.php
+++ index.php
@@ -410,7 +410,8 @@
           </div>
 
           <div class="col-12 col-sm-12">
-            <?php echo do_shortcode( '[contact-form-7 id="9" title="Trabaja con nosotros"]' ); ?>
+          <?php echo do_shortcode( '[contact-form-7 id="18" title="Trabaja con nosotros/"]' ); ?>
+            <!-- ?php echo do_shortcode( '[contact-form-7 id="9" title="Trabaja con nosotros"]' ); ?> -->
           </div>
         </div>
       </div>
